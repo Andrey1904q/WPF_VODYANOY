67654ef94539fbcf0ec07d1441553b82ca53f03e
234838 <voodser326@gmail.com>
2026-02-12 10:16:06 +0300

    Подключение SQL БД

---
+++ PR13/BDUtkin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR13
+{
+    internal class BDUtkin
+    {
+        public static dmitriyUtkinEntities Context = new dmitriyUtkinEntities();
+    }
+}
